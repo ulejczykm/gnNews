8e4a387049b7adbce948349b6bc3bcb62be5712e
ulejczykm <ulejczykm@gmail.com>
2023-03-31 21:29:04 +0200

    create NewsViewContainer component

--- gn-news-app/src/App.tsx
+++ gn-news-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "components/Header";
 import { Footer } from "components/Footer";
 import "./App.scss";
+import { NewsViewContainer } from "components/NewsViewContainer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NewsViewContainer />
       <Footer />
     </div>
   );
---
+++ gn-news-app/src/components/NewsViewContainer/index.tsx
@@ -0,0 +1,78 @@
+import { GridItem } from "components/GridItem";
+import "./styles.scss";
+import { ListItem } from "components/ListItem";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setArticlesNumber } from "store/slices/articlesNumberSlice";
+import { LIST } from "store/slices/newsViewSlice";
+import { RootState } from "store/store";
+
+interface NewsInterface {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: "2023-03-30T15:31:17Z";
+  source: { id: string; name: string };
+  title: string;
+  url: string;
+  urlToImage: string;
+}
+
+export const NewsViewContainer = () => {
+  const apiKey = process.env.REACT_APP_NEWS_API;
+  const { view } = useSelector((state: RootState) => state.newsView);
+  const [news, setNews] = useState<NewsInterface[]>([]);
+  const dispatch = useDispatch();
+  const containerStyle = view === LIST ? "newsList" : "newsGrid";
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch(
+          `https://newsapi.org/v2/top-headlines?country=pl&apiKey=${apiKey}`
+        );
+        if (!response.ok) {
+          throw new Error("Request failed with status " + response.status);
+        }
+        const data = await response.json();
+        setNews(data.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchNews();
+  }, []);
+
+  useEffect(() => {
+    dispatch(setArticlesNumber(news.length));
+  }, [news]);
+
+  console.log(news);
+
+  return (
+    <main className="mainContainer">
+      <ul className={containerStyle}>
+        {news.map(
+          ({ title, source, publishedAt, url, urlToImage, description }) =>
+            view === LIST ? (
+              <ListItem
+                key={url}
+                title={title}
+                name={source.name}
+                publishedAt={publishedAt}
+              />
+            ) : (
+              <GridItem
+                key={url}
+                title={title}
+                name={source.name}
+                publishedAt={publishedAt}
+                urlToImage={urlToImage}
+                description={description}
+              />
+            )
+        )}
+      </ul>
+    </main>
+  );
+};
