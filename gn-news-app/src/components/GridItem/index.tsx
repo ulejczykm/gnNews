a4d64e34f7a88fdb2a6abe38b5074c30b8c37518
ulejczykm <ulejczykm@gmail.com>
2023-03-31 21:28:13 +0200

    create GridItem component

---
+++ gn-news-app/src/components/GridItem/index.tsx
@@ -0,0 +1,27 @@
+import "./styles.scss";
+
+interface Props {
+  title: string;
+  name: string;
+  publishedAt: string;
+  urlToImage: string;
+  description: string;
+}
+
+export const GridItem = ({
+  title,
+  name,
+  publishedAt,
+  urlToImage,
+  description,
+}: Props) => (
+  <li className="gridItem">
+    <img src={urlToImage} alt={title} />
+    <div className="gridText">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <p>Źródło: {name}</p>
+      <p>Opublikowano:: {publishedAt}</p>
+    </div>
+  </li>
+);
