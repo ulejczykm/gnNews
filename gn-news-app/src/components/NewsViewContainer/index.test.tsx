ef0057e9a85484bf42db7e01893f09f382b71a0a
ulejczykm <ulejczykm@gmail.com>
2023-04-01 02:48:36 +0200

    remove unused code

--- gn-news-app/src/components/NewsViewContainer/index.test.tsx
+++ gn-news-app/src/components/NewsViewContainer/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { MemoryRouter, useParams } from "react-router-dom";
