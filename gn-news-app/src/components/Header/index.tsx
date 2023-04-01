7dc9362ead7c92d4ec6f0d18665ef2bc4a5fa7b0
ulejczykm <ulejczykm@gmail.com>
2023-04-01 03:20:43 +0200

    add description

--- gn-news-app/src/components/Header/index.tsx
+++ gn-news-app/src/components/Header/index.tsx
@@ -22,7 +22,18 @@ export const Header = () => {
           />
         }
       >
-        <p>test</p>
+        <p>
+          Największą trudność sprawiły mi na pewno testy, ponieważ nie używałem
+          ich zawodowo chyba od ponad półtora roku i okazuje się, że dużo rzeczy
+          musiałem sobie przypomnieć. A skoro paru rzeczy nie pamiętałem, to
+          oznacza, że to był odpowiedni czas na powtórkę materiału :D.
+          <br />
+          <br />
+          Kolejnym wyzawniem był Redux, ponieważ nigdy wcześniej go nie
+          używałem, a akurat od jakiegoś czasu zabierałem się za jego naukę :D.
+          I dlatego też, że mogłem w końcu nauczyć się Reduxa, zaliczam ten
+          element też jako największą frajdę.
+        </p>
       </Popup>
 
       <h1 className="title">
